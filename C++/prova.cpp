46f81751d06a5cd7d338850fde0bf9a5abfedfe3
Luca Ghislotti <ghislottiluca@gmail.com>
2021-08-16 08:37:52 +0200

    added first file
    Added first file to try out

---
+++ C++/prova.cpp
@@ -0,0 +1,15 @@
+/*
+ * prova.cpp
+ *
+ *  Created on: 16 ago 2021
+ *      Author: ghisl
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
